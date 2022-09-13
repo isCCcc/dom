4d47bef8421e6001091b7762258482d54f0ef333
isCCcc <1935158902@qq.com>
2022-09-13 23:10:38 +0800

    封装DOM的所有提供修改的API

--- dist/index.de158e3a.js
+++ dist/index.de158e3a.js
@@ -1,3 +1,4 @@
+// 测试
 const div = dom.create("<div><span>1<span></div>");
 console.log(div);
 dom.before(node, dom.create("<div>newNode-before</div>"));
@@ -5,5 +6,37 @@ dom.after(node, dom.create("<div>newNode-after</div>"));
 dom.wrap(nodeparent, dom.create('<div id="new-parent"></div>'));
 const empty = dom.empty(window.nodeempty);
 console.log(empty);
+const title = dom.attr(window.nodetitle, "title", "blanchecc");
+console.log(`title:${dom.attr(window.nodetitle, "title")}`);
+// text 测试
+const readme = window.readme;
+console.log(dom.text(readme));
+dom.text(readme, "This is the new content");
+console.log(dom.text(readme));
+// html 测试
+const readhtml = window.readhtml;
+console.log(dom.text(readhtml));
+dom.html(readhtml, "<span>This is the new HTML</span>");
+console.log(dom.text(readhtml));
+// style 测试
+const style = window.teststyle;
+dom.style(style, "font-size", "24px");
+dom.style(style, {
+    color: "red",
+    border: "1px solid pink"
+});
+console.log(dom.style(style, "color"));
+// class 测试
+const testclass = window.testclass;
+dom.class.add(testclass, "test");
+dom.class.remove(testclass, "old-class");
+console.log(dom.class.has(testclass, "old-class"));
+// 事件监听 测试
+const testListener = window.testListener;
+fn = ()=>{
+    console.log("事件被监听了");
+};
+dom.on(testListener, "click", fn);
+dom.off(testListener, "click", fn);
 
 //# sourceMappingURL=index.de158e3a.js.map
--- src/dom.js
+++ src/dom.js
@@ -1,42 +1,117 @@
-window.dom = {};
+window.dom = {
+    // 创建节点
+    create(string) {
+        const template = document.createElement('template');
+        template.innerHTML = string.trim();
+        return template.content.firstChild;
+    },
+    // 在指定节点之后插入节点
+    after(node, newNode) {
+        node.parentNode.insertBefore(newNode, node.nextSibling);
+    },
+    // 在指定节点之前插入节点
+    before(node, newNode) {
+        node.parentNode.insertBefore(newNode, node);
+    },
+    // 在指定节点里新增一个子节点
+    append(parent, node) {
+        parent.appendChild(node)
+    },
+    // 更换指定节点的父节点
+    wrap(node, parent) {
+        dom.before(node, parent);
+        dom.append(parent, node);
+    },
 
-// 创建节点
-dom.create = function (string) {
-    const template = document.createElement('template');
-    template.innerHTML = string.trim();
-    return template.content.firstChild;
-}
-// 在指定节点之后插入节点
-dom.after = function (node, newNode) {
-    node.parentNode.insertBefore(newNode, node.nextSibling);
-}
-// 在指定节点之前插入节点
-dom.before = function (node, newNode) {
-    node.parentNode.insertBefore(newNode, node);
-}
-// 在指定节点里新增一个子节点
-dom.append = function (parent, node) {
-    parent.appendChild(node)
-}
-// 更换指定节点的父节点
-dom.wrap = function (node, parent) {
-    dom.before(node, parent);
-    dom.append(parent, node);
-}
+
+    // 删除指定节点
+    remove(node) {
+        node.parentNode.removeChild(node)
+        return node
+    },
+    // 清空指点节点的所有子节点
+    empty(node) {
+        const array = [];
+        let x = node.firstChild
+        while (x) {
+            array.push(dom.remove(node.firstChild))
+            x = node.firstChild
+        }
+        return array
+    },
+
+    // 读写属性
+    attr(node, name, value) {
+        if (arguments.length === 3) {
+            node.setAttribute(name, value);
+        } else if (arguments.length === 2) {
+            return node.getAttribute(name)
+        }
+    },
+    // 读写文本属性
+    text(node, string) {
+        // 写操作：dom.text(div,'newTextContent')
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string // 兼容ie
+            } else {
+                node.textContent = string
+            }
+        } else if (arguments.length === 1) {  // 读操作dom.text(div)
+            if ('innerText' in node) {
+                return node.innerText
+            } else {
+                return node.textContent
+            }
+        }
+    },
+    // 读写html内容
+    html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
+    },
+    // 修改style内容
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            // dom.style(style, 'font-size', '24px');
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                // dom.style(style, 'color')
+                return node.style[name]
+            } else if (name instanceof Object) {
+                // dom.style(style, { color: 'red', border: '1px solid pink' })
+                for (let key in name) {
+                    node.style[key] = name[key]
+                }
+            }
+        }
+
+    },
+    // 操作标签里的class属性
+    class: {
+        add(node, className) {
+            node.classList.add(className);
+        },
+        remove(node, className) {
+            node.classList.remove(className);
+        },
+        has(node, className) {
+            return node.classList.contains(className);
+        }
+    },
+    // 用于添加事件监听
+    on(node, eventName, fn) {
+        node.addEventListener(eventName, fn);
+    },
+    // 用于删除事件监听
+    off(node, eventName, fn) {
+        node.removeEventListener(eventName, fn);
+    },
 
 
-// 删除指定节点
-dom.remove = function (node) {
-    node.parentNode.removeChild(node)
-    return node
-}
-// 清空指点节点的所有子节点
-dom.empty = function (node) {
-    const array = [];
-    let x = node.firstChild
-    while (x) {
-        array.push(dom.remove(node.firstChild))
-        x = node.firstChild
-    }
-    return array
+
 }
